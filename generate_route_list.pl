cd25d48c3b63a5dd74c74325af2ed7154a19d2d2
bruce.lu <lzbgt@126.com>
2014-12-11 00:36:25 +0800

    new

---
+++ generate_route_list.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Net::Netmask;
+ 
+sub _calc_netmask {
+    my($subnet) = @_;
+ 
+    # e.g.: 10.0.0.0/24 192.168.1.0/16
+    my $block = Net::Netmask->new($subnet);
+ 
+    
+    return($block->base(), $block->mask());
+}
+ 
+while(<>){
+    chomp;
+    my $line=$_;
+    my ($ip,$mask)= _calc_netmask($line);
+    printf("$ip $mask;");
+}
